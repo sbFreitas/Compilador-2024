3d74c77111b9845e41158364c2e520a63ec79496
sbFreitas <sarahb.freitas@gmail.com>
2024-08-29 19:08:21 -0300

    Fix

--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,27 @@
 namespace appCompilador;
 
 using System.Drawing;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 public partial class Form1 : Form
 {
+    [DllImport("user32.dll")]
+    private static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
+
+    [DllImport("user32.dll")]
+    private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+    private const int MOD_CONTROL = 0x0002;
+    private const int WM_HOTKEY = 0x0312;
+    private const int HOTKEY_ID_CTRL_N = 1;
+    private const int HOTKEY_ID_CTRL_S = 2;
+    private const int HOTKEY_ID_CTRL_O = 3;
+    private const int HOTKEY_ID_F7 = 4;
+    private const int HOTKEY_ID_F1 = 5;
+    private const int HOTKEY_ID_CTRL_C = 6;
+    private const int HOTKEY_ID_CTRL_V = 7;
+    private const int HOTKEY_ID_CTRL_X = 8;
 
     private bool newFile = true;
     private string path = "";
@@ -12,9 +29,102 @@ public partial class Form1 : Form
     public Form1()
     {
         InitializeComponent();
+        FormClosing += (s, e) =>
+        {
+            UnregisterHotKey(Handle, HOTKEY_ID_CTRL_N);
+            UnregisterHotKey(Handle, HOTKEY_ID_CTRL_S);
+            UnregisterHotKey(Handle, HOTKEY_ID_CTRL_O);
+            UnregisterHotKey(Handle, HOTKEY_ID_F7);
+            UnregisterHotKey(Handle, HOTKEY_ID_F1);
+            UnregisterHotKey(Handle, HOTKEY_ID_CTRL_C);
+            UnregisterHotKey(Handle, HOTKEY_ID_CTRL_V);
+            UnregisterHotKey(Handle, HOTKEY_ID_CTRL_X);
+        };
+
+        // Registrando CTRL + N
+        RegisterHotKey(Handle, HOTKEY_ID_CTRL_N, MOD_CONTROL, (int)Keys.N);
+        // Registrando CTRL + S
+        RegisterHotKey(Handle, HOTKEY_ID_CTRL_S, MOD_CONTROL, (int)Keys.S);
+        // Registrando CTRL + O
+        RegisterHotKey(Handle, HOTKEY_ID_CTRL_O, MOD_CONTROL, (int)Keys.O);
+        // Registrando F7
+        RegisterHotKey(Handle, HOTKEY_ID_F7, 0, (int)Keys.F7);
+        // Registrando F1
+        RegisterHotKey(Handle, HOTKEY_ID_F1, 0, (int)Keys.F1);
+        // Registrando CTRL + O
+        RegisterHotKey(Handle, HOTKEY_ID_CTRL_C, MOD_CONTROL, (int)Keys.C);
+        // Registrando CTRL + O
+        RegisterHotKey(Handle, HOTKEY_ID_CTRL_V, MOD_CONTROL, (int)Keys.V);
+        // Registrando CTRL + O
+        RegisterHotKey(Handle, HOTKEY_ID_CTRL_X, MOD_CONTROL, (int)Keys.X);
+
+
+        editor.TextChanged += Editor_TextChanged;
+        editor.VScroll += Editor_VScroll;
+
+        // Handle the panel's Paint event
+        panelLineNumbers.Paint += Panel_Paint;
+
+        // Handle the Layout event to update line numbers after the RichTextBox is fully laid out
+        editor.Layout += Editor_VScroll;
+
+        // Trigger line number drawing on form load
+        this.Load += Form1_Load;
+
+
+    }
+    protected override void WndProc(ref Message m)
+    {
+        if (m.Msg == WM_HOTKEY)
+        {
+            int id = m.WParam.ToInt32();
+
+            if (id == HOTKEY_ID_CTRL_N)
+            {
+                // Handle CTRL + N hotkey
+                ButtonNew_Click(this, EventArgs.Empty);
+            }
+            else if (id == HOTKEY_ID_CTRL_O)
+            {
+                // Handle CTRL + O hotkey
+                ButtonOpen_Click(this, EventArgs.Empty);
+            }
+            if (id == HOTKEY_ID_CTRL_S)
+            {
+                // Handle CTRL + S hotkey
+                ButtonSave_Click(this, EventArgs.Empty);
+            }
+            else if (id == HOTKEY_ID_F7)
+            {
+                // Handle F7 hotkey
+                ButtonCompile_Click(this, EventArgs.Empty);
+            }
+            else if (id == HOTKEY_ID_F1)
+            {
+                // Handle F7 hotkey
+                ButtonTeam_Click(this, EventArgs.Empty);
+            }
+            else if (id == HOTKEY_ID_CTRL_C)
+            {
+                // Handle CTRL + C hotkey
+                ButtonCopy_Click(this, EventArgs.Empty);
+            }
+            else if (id == HOTKEY_ID_CTRL_V)
+            {
+                // Handle CTRL + V hotkey
+                ButtonPaste_Click(this, EventArgs.Empty);
+            }
+            else if (id == HOTKEY_ID_CTRL_X)
+            {
+                // Handle CTRL + X hotkey
+                ButtonCut_Click(this, EventArgs.Empty);
+            }
+        }
+
+        base.WndProc(ref m);
     }
 
-    private void Form1_Load(object sender, EventArgs e)
+    private void Form1_Load(object? sender, EventArgs e)
     {
 
     }
@@ -26,65 +136,49 @@ public partial class Form1 : Form
         statusBarLabel.Text = "";
     }
 
-    private void richTextBox1_TextChanged(object sender, EventArgs e)
+    private void Editor_TextChanged(object? sender, EventArgs e)
     {
-        panel.Refresh();
+        UpdateLineNumbers();
     }
 
-    private void RichTextBox1_VScroll(object sender, EventArgs e)
+    private void Editor_VScroll(object? sender, EventArgs e)
     {
-        panel.Refresh();
+        panelLineNumbers.Invalidate();
     }
-
-    private void panel_Paint(object sender, PaintEventArgs e)
+    private void UpdateLineNumbers()
     {
-        //TODO: arrumar numeração das linhas
+        // Get the first visible line index
+        int firstIndex = editor.GetCharIndexFromPosition(new Point(0, 0));
+        int firstLine = editor.GetLineFromCharIndex(firstIndex);
 
-        Font font = editor.Font;
-        int linhaAtual = editor.GetLineFromCharIndex(editor.SelectionStart);
-        int linhasVisiveis = editor.ClientSize.Height / font.Height;
+        // Get the total number of lines in the RichTextBox
+        int totalLines = editor.Lines.Length;
 
-        for (int i = 0; i < linhasVisiveis; i++)
+        // Draw the line numbers
+        using (Graphics g = panelLineNumbers.CreateGraphics())
         {
-            string numeroLinha = (linhaAtual + i + 1).ToString();
-            e.Graphics.DrawString(numeroLinha, font, Brushes.Black, 2, i * font.Height);
+            g.Clear(panelLineNumbers.BackColor);
+
+            for (int i = firstLine; i < totalLines; i++)
+            {
+                int y = editor.GetPositionFromCharIndex(editor.GetFirstCharIndexFromLine(i)).Y;
+
+                g.DrawString((i + 1).ToString(), editor.Font, Brushes.Black, new PointF(0, y));
+            }
         }
     }
 
-    //TODO: implementar chamada para as teclas de atalhos
-    private void button_KeyDown(object? sender, KeyEventArgs e)
+    private void Panel_Paint(object? sender, PaintEventArgs e)
     {
-        if (e.Control && e.KeyCode == Keys.N)
-            buttonNew_Click(sender, e);
-
-        else if (e.Control && e.KeyCode == Keys.O)
-            buttonOpen_Click(sender, e);
-
-        else if (e.Control && e.KeyCode == Keys.S)
-            buttonSave_Click(sender, e);
-
-        else if (e.Control && e.KeyCode == Keys.C)
-            ButtonCopy_Click(sender, e);    //TODO: criar método para copiar
-
-        else if (e.Control && e.KeyCode == Keys.V)
-            ButtonPaste_Click(sender, e);    //TODO: criar método para colar
-
-        else if (e.Control && e.KeyCode == Keys.X)
-            ButtonCut_Click(sender, e);    //TODO: criar método para cortar
-
-        else if (e.KeyCode == Keys.F7)
-            throw new NotImplementedException();    //TODO: criar método para compilar
-
-        else if (e.KeyCode == Keys.F1)
-            throw new NotImplementedException();    //TODO: criar método para mostrar equipe
+        UpdateLineNumbers();
     }
 
-    private void buttonNew_Click(object sender, EventArgs e)
+    private void ButtonNew_Click(object? sender, EventArgs e)
     {
         ClearAll();
     }
 
-    private void buttonOpen_Click(object sender, EventArgs e)
+    private void ButtonOpen_Click(object? sender, EventArgs e)
     {
         OpenFileDialog openFileDialog = new OpenFileDialog();
         openFileDialog.Filter = "Arquivos de Texto (*.txt)|*.txt|Todos os arquivos (*.*)|*.*";
@@ -104,7 +198,7 @@ public partial class Form1 : Form
         }
     }
 
-    private void buttonSave_Click(object sender, EventArgs e)
+    private void ButtonSave_Click(object? sender, EventArgs e)
     {
         if (newFile && path == "")
         {
@@ -115,7 +209,7 @@ public partial class Form1 : Form
             {
                 try
                 {
-                    editor.SaveFile(saveFileDialog.FileName);
+                    editor.SaveFile(saveFileDialog.FileName, RichTextBoxStreamType.PlainText);
                     path = saveFileDialog.FileName;
                     newFile = false;
 
@@ -133,7 +227,7 @@ public partial class Form1 : Form
         {
             try
             {
-                editor.SaveFile(path);
+                editor.SaveFile(path, RichTextBoxStreamType.PlainText);
                 messageArea.Clear();
             }
             catch (Exception ex)
@@ -143,7 +237,7 @@ public partial class Form1 : Form
         }
     }
 
-    private void ButtonCopy_Click(object sender, EventArgs e)
+    private void ButtonCopy_Click(object? sender, EventArgs e)
     {
         if (editor.SelectedText != "")
         {
@@ -151,7 +245,7 @@ public partial class Form1 : Form
         }
     }
 
-    private void ButtonPaste_Click(object sender, EventArgs e)
+    private void ButtonPaste_Click(object? sender, EventArgs e)
     {
         if (Clipboard.ContainsText())
         {
@@ -159,7 +253,7 @@ public partial class Form1 : Form
         }
     }
 
-    private void ButtonCut_Click(object sender, EventArgs e)
+    private void ButtonCut_Click(object? sender, EventArgs e)
     {
         if (editor.SelectedText != "")
         {
@@ -167,13 +261,20 @@ public partial class Form1 : Form
         }
     }
 
-    private void buttonCompile_Click(object sender, EventArgs e)
+    private void ButtonCompile_Click(object? sender, EventArgs e)
     {
-
+        messageArea.Clear();
+        messageArea.Text = "compilação de programas ainda não foi implementada";
     }
 
-    private void buttonTeam_Click(object sender, EventArgs e)
+    private void ButtonTeam_Click(object? sender, EventArgs e)
     {
+        messageArea.Clear();
+        messageArea.Text = "Pedro, Marlon e Sarah";
+    }
 
+    private void Editor_Layout(object? sender, LayoutEventArgs e)
+    {
+        UpdateLineNumbers();
     }
 }
